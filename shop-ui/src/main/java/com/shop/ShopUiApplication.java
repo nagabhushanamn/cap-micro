f2d33e01a31af3a17033dc93333a0388addcc332
nag-training <nag-training@172.20.10.2>
2019-05-26 10:55:50 +0530

    shop ui

--- shop-ui/src/main/java/com/shop/ShopUiApplication.java
+++ shop-ui/src/main/java/com/shop/ShopUiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShopWebUiApplication {
+public class ShopUiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShopWebUiApplication.class, args);
+		SpringApplication.run(ShopUiApplication.class, args);
 	}
 
 }
--- shop-ui/src/test/java/com/shop/ShopUiApplicationTests.java
+++ shop-ui/src/test/java/com/shop/ShopUiApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ShopWebUiApplicationTests {
+public class ShopUiApplicationTests {
 
 	@Test
 	public void contextLoads() {
